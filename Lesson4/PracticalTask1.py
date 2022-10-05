4f4b4acda0b991d725966ee3249cdd34ada7bdf6
nadezhda <nadezhda.bessonova@gmail.com>
2022-10-05 20:27:43 +0500

    Урок 4. Знакомство с Python. Задача 2

---
+++ Lesson4/PracticalTask1.py
@@ -0,0 +1,10 @@
+# 1. Вычислить число c заданной точностью d
+# in                                                      in
+# Enter a real number: 9                                  Enter a real number: 8.98785
+# Enter the required accuracy '0.0001': 0.000001          Enter the required accuracy '0.0001': 0.001
+# out                                                     out
+# 9.000000                                                8.988
+
+
+
+
