98927879bc8bad45e24cb5495f3daf7efca43a1e
nadezhda <nadezhda.bessonova@gmail.com>
2022-10-07 14:49:12 +0500

    Урок 4. Знакомство с Python. Задача 4

--- Lesson4/PracticalTask4.py
+++ Lesson4/PracticalTask4.py
@@ -19,3 +19,18 @@
 # 4*x^2 - 4 = 0
 # 2*x^4 - 3*x^3 + 3*x^2 + 1*x^1 - 2 = 0
 
+from random import randint
+from secrets import choice
+
+def creating_a_file (num): # создание файла с многочленом
+    if num <= 0:
+        return print('Ошибка, введите положительное число больше ноля')
+    with open('file.txt', 'a', encoding='utf-8') as my_file:
+        while num > 0:
+            num_ran =randint(0, 10)
+            if num_ran != 0:
+                my_file.write(f'{num_ran}*x^{num} {choice("+-")} ')
+            num -= 1
+        else: my_file.write(f'{randint(0, 10)} = 0\n')  
+    
+creating_a_file(int(input('Введите степень многочлена: ')))
