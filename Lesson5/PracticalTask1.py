561ff638b375fd9e4d03e1a3c2e20253a58a43a6
nadezhda <nadezhda.bessonova@gmail.com>
2022-10-10 15:17:52 +0500

    Урок 5. Знакомство с Python. Задача 1

--- Lesson5/PracticalTask1.py
+++ Lesson5/PracticalTask1.py
@@ -16,3 +16,30 @@
 # out
 # The data is incorrect
 
+from random import choices
+
+def list_new (n, word):  #создание списка слов
+    if n <= 0:
+        return "Данные не верны, введите целое положительное число!"
+    new_list = []
+    for i in  range(n):
+        a = choices(word, k=3)  #возвращение элемента из 3х переменных
+        new_list.append(''.join(a))
+    return new_list
+
+def list_edit (text):  # Скорректированный техт не содержащий "абв"
+    new_text = []
+    i = 0
+    while i < len(text):
+        if not(text[i] == 'а' and text[i+1] == 'б' and text[i+2] == 'в'):
+            new_text.append(''.join(text[i] + text[i+1] + text[i+2]))
+        i += 4
+    return new_text        
+                
+
+
+list = " ".join(list_new(int(input('Введите количество слов: ')), "абв"))
+print(list)
+new_list = " ".join(list_edit(list))
+print('Скорректированный техт не содержащий "абв": ')
+print(new_list)
