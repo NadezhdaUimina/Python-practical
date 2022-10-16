1a3d4742db0b002bb07b61ec06a56cd6550b6dff
nadezhda <nadezhda.bessonova@gmail.com>
2022-10-16 17:17:10 +0500

    Урок 7. Знакомство с Python. README

---
+++ Lesson7/user_interface.py
@@ -0,0 +1,7 @@
+def info ():
+    info = []
+    info.append(input('Введите фамилию: '))
+    info.append(input('Введите имя: '))
+    info.append(input('Введите номер телефона: '))
+    info.append(input('Введите описание: '))
+    return infohm
