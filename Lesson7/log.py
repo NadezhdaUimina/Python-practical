6b8b82b15b3c3e2bb58fea8cdb6765d113e1dd08
nadezhda <nadezhda.bessonova@gmail.com>
2022-10-17 18:50:46 +0500

    Урок 7. Телефонный справочник

--- Lesson7/log.py
+++ Lesson7/log.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from time import time
 
 
-def div_log(data):
+def log(data):
     time = datetime.now().strftime('%H:%M')
     with open('log.csv', 'a') as log_file:
         log_file.write('{} / {} = {} Time {}\n'.format(data, time))
