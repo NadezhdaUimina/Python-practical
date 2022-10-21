b9b8dd79e8c271c6f3258e71cad2ece088e01898
nadezhda <nadezhda.bessonova@gmail.com>
2022-10-21 20:09:45 +0500

    Урок 7. Телефонный справочник

--- Lesson7/search.py
+++ Lesson7/search.py
@@ -11,9 +11,9 @@ def search():
             for i, v in enumerate(text_csv):
                 if search in v:
                     print('Данные из телефонного справочника в фаиле Lesson7/Phone_book.csv')
-                    print(v.strip())
+                    print(f'{v.strip()}\n')
                     count = True
-            if not count: print('Таких данных нет в справочнике фаила Lesson7/Phone_book.csv')
+            if not count: print('Таких данных нет в справочнике фаила Lesson7/Phone_book.csv\n')
 
     file = 'Lesson7/Phone_book.txt'
     if path.exists(file):
@@ -22,7 +22,9 @@ def search():
             text_txt = text.readlines()
             for i, v in enumerate(text_txt):
                 if search in v:
+                    while i == '':
+                        i -= 1
                     print('Данные из телефонного справочника в фаиле Lesson7/Phone_book.txt')
-                    print(f'{text_txt[i]}{text_txt[i+1]}{text_txt[i+2]}{text_txt[i+3]}' )
+                    print(f'{text_txt[i-1]}{text_txt[i]}{text_txt[i+1]}{text_txt[i+2]}\n' )
                     count = True
-            if not count: print('Таких данных нет в справочнике фаила Lesson7/Phone_book.txt')
+            if not count: print('Таких данных нет в справочнике фаила Lesson7/Phone_book.txt\n')
